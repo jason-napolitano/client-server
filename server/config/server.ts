e2636bbf9af727a633ae6f2db4481bf20e6c0b72
Jason Napolitano <jason-napolitano@users.noreply.github.com>
2021-05-03 07:16:46 -0700

    Update server.ts

--- server/config/server.ts
+++ server/config/server.ts
@@ -1,3 +1,12 @@
 export default {
-  title: "My amazing Typescript + Express powered API!"
+  title: "My amazing Typescript + Express powered API!",
+  corsOptions: {
+    origin: [
+      'http://localhost:8080',
+      'http://localhost:5000'
+    ],
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    preflightContinue: false,
+    optionsSuccessStatus: 204
+  }
 }
