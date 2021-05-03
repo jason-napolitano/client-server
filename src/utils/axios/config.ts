4863462637a563369c564a3fbb1a63f9e09a1cb5
Jason Napolitano <jason-napolitano@users.noreply.github.com>
2021-05-03 07:15:44 -0700

    Update config.ts

--- src/utils/axios/config.ts
+++ src/utils/axios/config.ts
@@ -2,7 +2,7 @@ const config = {
   // `baseURL` will be prepended to `url` unless `url` is absolute.
   // It can be convenient to set `baseURL` for an instance of axios to pass relative URLs
   // to methods of that instance.
-  baseURL: `http://localhost:${process.env.SERVER_PORT || 3001}/`
+  baseURL: `http://localhost:${process.env.SERVER_PORT || 4900}/`
 }
 
 export { config }
